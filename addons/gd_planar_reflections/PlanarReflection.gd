dd45d8565c1f34aa8906e38d14f31b4830eb8023
Florian <e.k.florian@gmail.com>
2025-02-09 02:26:16 +0100

    repair project, add grass, edit env apsur

--- addons/gd_planar_reflections/PlanarReflection.gd
+++ addons/gd_planar_reflections/PlanarReflection.gd
@@ -23,8 +23,8 @@ func _ready():
 		reflect_camera.oblique_normal = Vector3.UP;
 	reflect_camera.cull_mask = 1;
 	reflect_camera.fov = main_cam.fov
-	reflect_camera.environment = main_cam.environment
-	reflect_camera.attributes = main_cam.attributes
+	#reflect_camera.environment = main_cam.environment
+	#reflect_camera.attributes = main_cam.attributes
 	reflect_camera.doppler_tracking = main_cam.doppler_tracking
 	reflect_camera.projection = main_cam.projection
 	reflect_camera.current = true;

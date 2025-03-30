921307bba15ed65193b9768ab67c1ec5f83e8ad9
Florian <e.k.florian@gmail.com>
2025-03-30 19:11:16 +0200

    rework grass arround particle shader

--- ressources/grass/grass_particule_terrain3d.gd
+++ ressources/grass/grass_particule_terrain3d.gd
@@ -5,10 +5,23 @@ extends GPUParticles3D
 	set(value):
 		terrain = value
 		_update_process_parameters()
+		
+@export var player: Node3D :
+	set(value):
+		player = value
+		_update_process_parameters()
 
 func _update_process_parameters() -> void:
 	var process_rid: RID = process_material.get_rid()
 	if terrain:
+		var camera := terrain.get_camera()
+		if camera:
+			global_position.x = floorf(camera.global_position.x)
+			global_position.z = floorf(camera.global_position.z)
+			if(player) :
+				RenderingServer.material_set_param(process_rid, "camera_transform", player.transform)
+			else :
+				RenderingServer.material_set_param(process_rid, "camera_transform", camera.get_camera_transform())
 		RenderingServer.material_set_param(process_rid, "_background_mode", terrain.material.world_background)
 		RenderingServer.material_set_param(process_rid, "_vertex_spacing", terrain.vertex_spacing)
 		RenderingServer.material_set_param(process_rid, "_vertex_density", 1.0 / terrain.vertex_spacing)
@@ -20,13 +33,10 @@ func _update_process_parameters() -> void:
 		RenderingServer.material_set_param(process_rid, "_height_maps", terrain.data.get_height_maps_rid())
 		RenderingServer.material_set_param(process_rid, "_control_maps", terrain.data.get_control_maps_rid())
 		
+		
 func _ready() -> void:
 	_update_process_parameters()
 
 func _physics_process(delta: float) -> void:
-	if terrain:
-		_update_process_parameters() # lazy but catches vertex spacing changes
-		var camera := terrain.get_camera()
-		if camera:
-			global_position.x = floorf(camera.global_position.x)
-			global_position.z = floorf(camera.global_position.z)
+	_update_process_parameters() # lazy but catches vertex spacing changes
+	
